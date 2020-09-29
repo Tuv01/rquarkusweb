ed3586517eec06a1abf366b0177ba05647823d16
Tuv01 <tuvilla.jesus@gmail.com>
2020-09-29 14:14:10 +0000

    d

--- src/main/java/org/tuv01/GreetingResource.java
+++ src/main/java/org/tuv01/GreetingResource.java
@@ -1,6 +1,9 @@
 package org.tuv01;
 
 import org.rest.json.Fruit;
+
+import java.util.UUID;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -14,11 +17,13 @@ import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
+
+//JAX-RS EndPoint 
 @Path("/hello")
 public class GreetingResource {
 
     public static final String TOKEN= "dev";
-    
+
     @Inject
     GreetingService service;
     
@@ -31,9 +36,9 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/ok")
+    @Path("/html")
     public String hello() {
-        return "hello";
+        return "hello" + UUID.randomUUID().toString();
     }
 
     @GET
@@ -57,11 +62,15 @@ public class GreetingResource {
     }
 
     @POST
-    @Path("/addToken")
+    @Path("/token")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public String addHello(@HeaderParam("token")String hName,@QueryParam("token") String aName, String body){
-        return "{\"key\": \"" + aName + "\"}";
+        String token = hName != null ? hName: aName;
+        if (!TOKEN.equals(token)){
+            throw new RuntimeException("Unauthorized");
+        }
+        return "<b>Text<b>";
     }
 
 }
