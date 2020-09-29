1001996ebe45e107f4eab83b59b46a8ec412a1fd
Tuv01 <tuvilla.jesus@gmail.com>
2020-09-29 13:26:40 +0000

    see using Hoppscotch

--- src/main/java/org/tuv01/GreetingResource.java
+++ src/main/java/org/tuv01/GreetingResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -21,7 +22,7 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
     public String greeting(@PathParam String name){
-        return service.greeting(name);
+        return service.greeting(" " +name);
     }
 
     @GET
@@ -49,6 +50,13 @@ public class GreetingResource {
         String output = fruit.toString();
  
         return Response.status(200).entity(output).build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String addHello(@QueryParam("name") String aName, String body){
+        return "{\"key\": \"" + aName + "\"}";
     }
 
 }
