70de39c2ee132b84d213d9e0a6c82c6c74c00130
Tuv01 <tuvilla.jesus@gmail.com>
2020-09-30 11:53:26 +0000

    refactor and added logic

--- src/main/java/org/greeting/GreetingResource.java
+++ src/main/java/org/greeting/GreetingResource.java
@@ -1,76 +1,24 @@
 package org.greeting;
 
-import org.rest.json.Fruit;
-
-import java.util.UUID;
-
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
 
 //JAX-RS EndPoint 
 @Path("/hello")
 public class GreetingResource {
 
-    public static final String TOKEN= "dev";
 
     @Inject
-    GreetingService service;
-    
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/greeting/{name}")
-    public String greeting(@PathParam String name){
-        return service.greeting(" " +name);
-    }
+    private GreetingService greetingService;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/html")
-    public String hello() {
-        return "hello" + UUID.randomUUID().toString();
-    }
-
-    @GET
-    @Path("/print/{name}")
-    @Produces("application/json")
-    public Fruit produceJSON( @PathParam("name") String name ) {
- 
-        Fruit fruit = new Fruit(name);
- 
-        return fruit;
-    }
-    
-    @POST
-    @Path("/send")
-    @Consumes("application/json")
-    public Response consumeJSON( Fruit fruit) {
- 
-        String output = fruit.toString();
- 
-        return Response.status(200).entity(output).build();
-    }
-
-    @POST
-    @Path("/token")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String addHello(@HeaderParam("token")String hName,@QueryParam("token") String aName, String body){
-        String token = hName != null ? hName: aName;
-        if (!TOKEN.equals(token)){
-            throw new RuntimeException("Unauthorized");
-        }
-        return "<b>Text<b>";
+    public String hello(@QueryParam("name") String name) {
+        return greetingService.greeting(name);
     }
 
 }
--- src/main/java/org/greeting/GreetingService.java
+++ src/main/java/org/greeting/GreetingService.java
@@ -1,11 +1,17 @@
 package org.greeting;
 
+import java.util.UUID;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class GreetingService {
     
     public String greeting (String name){
-        return "hello" + name;
+        return String.format("Hello %s, your id is %s",
+        name, 
+        UUID.randomUUID().toString()
+        );
     }
+
 }
