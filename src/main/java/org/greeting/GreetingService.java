bc57e541d0c0ea44f77242cd72b87ce1ceb2b2b0
Tuv01 <tuvilla.jesus@gmail.com>
2020-10-01 15:09:52 +0000

    minor

--- src/main/java/org/greeting/GreetingService.java
+++ src/main/java/org/greeting/GreetingService.java
@@ -6,17 +6,21 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.config.GreetingConfig;
+import org.jboss.logging.Logger;
 
 
 @ApplicationScoped
 public class GreetingService {
     
+    private static Logger LOGGER = Logger.getLogger(GreetingService.class.getName());
+
     //A package-private constructor injection. @Inject is optional in this particular case.
     @Inject
     private GreetingConfig greetingConfig;
   
 
     public String greeting (String name){
+        LOGGER.debug("Saying hello to random user");
         return String.format("Hello %s, your id is %s",
         name, 
         UUID.randomUUID().toString()
