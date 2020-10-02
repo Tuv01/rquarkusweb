60a61d045da306f141e9dc43cad8e0678467f434
Tuv01 <tuvilla.jesus@gmail.com>
2020-10-02 13:26:19 +0000

    added JsonbConfigCustomizer

--- src/main/java/org/books/BookResource.java
+++ src/main/java/org/books/BookResource.java
@@ -1,7 +1,6 @@
 package org.books;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -15,48 +14,53 @@ import javax.ws.rs.core.*;
 
 import org.data.Book;
 
-
-
 @Path("/book")
 public class BookResource {
 
-    private static ArrayList <Book> books = new ArrayList<>();
+    private static ArrayList<Book> books = new ArrayList<>();
 
     static {
-        books.add(new Book("The Freelancer's Bible: Everything You Need to Know to Have the Career of Your Dreams","Sara Horowitz"));
-        books.add(new Book("building-an-api-backend-with-microprofile","Hayri Cicek"));
-        books.add(new Book("Microservices best practices","IBM"));
+        books.add(new Book("The Freelancer's Bible: Everything You Need to Know to Have the Career of Your Dreams",
+                "Sara Horowitz"));
+        books.add(new Book("building-an-api-backend-with-microprofile", "Hayri Cicek"));
+        books.add(new Book("Microservices best practices", "IBM"));
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Book> getBooks(){
-        return books;
+    public Response getBooks() {
+        return Response.status(202).entity(books).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Book addBook (Book book){
-        books.add(book);
-        return book;
+    public Response addBook(Book book) {
+
+        if (books.size() > 5) {
+            return Response.status(400).entity("No more than 5 books are allowed").build();
+        } else {
+            books.add(book);
+            return Response.ok(book).build();
+        }
+
     }
 
     @PUT
     @Path("/{id}")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public Book updateBook(@PathParam("id") Integer index, Book book){
+    public Book updateBook(@PathParam("id") Integer index, Book book) {
         books.remove((int) index);
-        books.add(index,book);
+        books.add(index, book);
         return book;
     }
 
     @DELETE
     @Path("/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public Book deleteBook(@PathParam("id") Integer index){
-        return books.remove((int)index);
+    public Book deleteBook(@PathParam("id") Integer index) {
+        return books.remove((int) index);
     }
 
 }
---
+++ src/main/java/org/config/JsonConverterConfig.java
@@ -0,0 +1,15 @@
+package org.config;
+
+import javax.json.bind.JsonbConfig;
+
+import io.quarkus.jsonb.JsonbConfigCustomizer;
+
+public class JsonConverterConfig implements JsonbConfigCustomizer {
+
+    @Override
+    public void customize(JsonbConfig jsonbConfig) {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
